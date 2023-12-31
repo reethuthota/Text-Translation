f2e92b704ecc0cf14cffc37aee3b9a088917f6c1
reethuthota <reethu.thota@gmail.com>
2023-12-31 19:50:11 +0530

    Create langchain_helper.py

---
+++ langchain_helper.py
@@ -0,0 +1,23 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from secret_key import openapi_key
+
+import os
+os.environ['OPENAI_API_KEY'] = openapi_key
+
+llm = OpenAI(temperature=0.7)
+
+def generate_translation(language, text):
+    translation_prompt = PromptTemplate(
+        input_variables=['language', 'text'],
+        template="Translate {text} to {language}"
+    )
+
+    chain = LLMChain(llm=llm, prompt=translation_prompt, output_key="translatedText")
+    response = chain({'language': language, 'text': text})
+    return response
+
+if __name__ == "__main__":
+    print(generate_translation("French","Hello"))
