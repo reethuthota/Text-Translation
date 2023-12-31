7c3366030d660e412db8357e3a52ff09e85d01a4
reethuthota <reethu.thota@gmail.com>
2023-12-31 20:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ language = st.selectbox("Pick a Language to translate into", ("English", "French
 text = st.text_input("Enter Text to be Translated")
 
 if language and text:
-    response = langchain_helper.generate_translation(language, text)
-    translated = response['translatedText'].strip()
+    translation = langchain_helper.generate_translation(language, text)
+    translated = translation['translatedText'].strip()
     st.write(translated)
         
 # run using "streamlit run app.py"
