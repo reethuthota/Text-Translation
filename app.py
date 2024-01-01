18a476f2f55ea7949c2c286518e4387960a0a9af
reethuthota <reethu.thota@gmail.com>
2024-01-01 13:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import evaluation
 
 st.title("Text Translator")
 
-language = st.selectbox("Pick a Language to translate into", ("English", "French"))
+from_language = st.selectbox("Translate from", ("English", "French"))
+to_language = st.selectbox("Translate to", ("English", "French"))
+
+if from_language == to_language:
+    st.warning("Please select different languages for translation.")
+    st.stop()
 
 text = st.text_input("Enter Text to be Translated")
 
-if language and text:
-    translation = langchain_helper.generate_translation(language, text)
-    translated = translation['translatedText'].strip()
-    st.write(translated)
-    
-    similarity = evaluation.similarity(translated)
-    st.write("Accuracy :",similarity)
-        
-# run using "streamlit run app.py"
+if from_language and to_language and text:
+    translated = langchain_helper.generate_translation(to_language, text)
+    st.write(translated['translatedText'].strip())
+
+    similarity = evaluation.similarity(translated['translatedText'].strip())
+    st.write("Accuracy:", similarity)
